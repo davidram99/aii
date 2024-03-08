a349405e8d6368f397a91f45a45ee3b92b15a167
davramgar <david.ram@outlook.es>
2024-03-08 19:46:46 +0100

    Falta el ultimo

--- Lab_2/src/main.py
+++ Lab_2/src/main.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import requests
 import tkinter as tk
 from tkinter import messagebox
-from tkinter import Toplevel, Listbox, Scrollbar, END, Y, BOTH, RIGHT, LEFT
+from tkinter import Toplevel, Listbox, Scrollbar, END, Y, BOTH, RIGHT, LEFT, ttk
 import sqlite3
 # lineas para evitar error SSL
 import os, ssl
@@ -57,7 +57,7 @@ def almacenar_bd():
     messagebox.showinfo("Base Datos", "Base de datos creada correctamente \nHay " + str(cursor.fetchone()[0]) + " registros")
     conn.close()
 
-def buscar_por_jornada(numero):
+def buscar_y_mostrar_resultados(numero):
     try:
         # Conexión a la base de datos
         conn = sqlite3.connect('resultados.db')
@@ -65,12 +65,64 @@ def buscar_por_jornada(numero):
         cursor.execute("SELECT * FROM jornadas WHERE numero = ?", (numero,))
         # Lo convertimos en una lista
         data = cursor.fetchall()
+        # Creamos la ventana tipo listbox con scroll
+        ventana = Toplevel()
+        listbox = Listbox(ventana, width=50)
+        scrollbar = Scrollbar(ventana)
+        scrollbar.pack(side=RIGHT, fill=Y)
+        listbox.pack(side=LEFT, fill=BOTH, expand=1)
+        scrollbar.config(command=listbox.yview)
+        listbox.config(yscrollcommand=scrollbar.set)
+        # Añadimos los partidos
+        for partido in data:
+            listbox.insert(END, f'{partido[1]} {partido[3]} {partido[2]}')
         # Cerramos la conexión
         conn.close()
-        return data
     except Exception as e:
         messagebox.showerror("Error", "Error al buscar en la base de datos")
-        return []
+
+def buscar_y_mostrar_estadisticas(numero):
+    try:
+        # Conexión a la base de datos
+        conn = sqlite3.connect('resultados.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM jornadas WHERE numero = ?", (numero,))
+        # Lo convertimos en una lista
+        data = cursor.fetchall()
+        # Creamos la ventana tipo listbox con scroll
+        ventana = Toplevel()
+        listbox = Listbox(ventana, width=50)
+        scrollbar = Scrollbar(ventana)
+        scrollbar.pack(side=RIGHT, fill=Y)
+        listbox.pack(side=LEFT, fill=BOTH, expand=1)
+        scrollbar.config(command=listbox.yview)
+        listbox.config(yscrollcommand=scrollbar.set)
+        goles_jornada = 0
+        empates_jornada = 0
+        victorias_local_jornada = 0
+        victorias_visitante_jornada = 0  
+        for partido in data:
+            # Total de goles de la jornada
+            goles = partido[3].split('-')
+            print(goles)
+            goles_jornada += int(goles[0]) + int(goles[1])
+            # Empates
+            if int(goles[0]) == int(goles[1]):
+                empates_jornada += 1
+            # Victorias locales
+            elif int(goles[0]) > int(goles[1]):
+                victorias_local_jornada += 1
+            # Victorias visitantes
+            else:
+                victorias_visitante_jornada += 1
+        listbox.insert(END, f'TOTAL GOLES JORNADA: {goles_jornada}')
+        listbox.insert(END, '')
+        listbox.insert(END, f'EMPATES: {empates_jornada}')
+        listbox.insert(END, f'VICTORIAS LOCALES: {victorias_local_jornada}')
+        listbox.insert(END, f'VICTORIAS VISITANTES: {victorias_visitante_jornada}')
+
+    except Exception as e:
+        messagebox.showerror("Error", "Error al buscar en la base de datos")
 
 def listar_jornadas():
     # Conexión a la base de datos
@@ -100,51 +152,74 @@ def listar_jornadas():
         listbox.insert(END, "")
     conn.close()
     
-def buscar_jornada():
+def buscar_jornada(tipo_busqueda):
+    # Conectamos a la base de datos
+    conn = sqlite3.connect('resultados.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT DISTINCT numero FROM jornadas")
+    # Lo convertimos en una lista
+    data = cursor.fetchall()
     # Creamos la ventana
     ventana = Toplevel()
     ventana.title("Buscar Jornada")
     # Creamos el label
-    label = tk.Label(ventana, text="Introduce el número de la jornada")
+    label = tk.Label(ventana, text="Seleccione la jornada")
     label.pack()
+    # Jornada será una lista con los números de las jornadas (el primer elemento de cada set de la lista data)
+    jornadas = [jornada[0] for jornada in data]
+    # Ordena la lista
+    jornadas.sort()
     # Creamos el entry de número de jornada que solo acepta números entre 1 y 38
     entry = tk.Entry(ventana)
     entry.pack()
-    entry.config(validate="key", validatecommand=(entry.register(lambda text: text.isdigit() and 0 < int(text) < 39), "%P"))
-    # Función para llamar a la función buscar_por_jornada y guardar el resultado en una variable
-    resultados = []
-    def buscar_y_guardar():
-        nonlocal resultados
-        resultados = buscar_por_jornada(entry.get())
-            # Creamos la ventana tipo listbox con scroll
-        ventana = Toplevel()
-        listbox = Listbox(ventana, width=50)
-        scrollbar = Scrollbar(ventana)
-        scrollbar.pack(side=RIGHT, fill=Y)
-        listbox.pack(side=LEFT, fill=BOTH, expand=1)
-        scrollbar.config(command=listbox.yview)
-        listbox.config(yscrollcommand=scrollbar.set)
-        # Añadimos las jornadas
-        for partido in resultados:
-            listbox.insert(END, f'{partido[1]} {partido[3]} {partido[2]}')
+    entry.config(validate="key", validatecommand=(entry.register(lambda text: text.isdigit() and jornadas[0] <= int(text) <= jornadas[-1] or text == ""), "%P"))
     # Creamos el boton
-    boton = tk.Button(ventana, text="Buscar", command=buscar_y_guardar)
+    if tipo_busqueda == "resultados":
+        boton = tk.Button(ventana, text="Buscar", command = lambda:buscar_y_mostrar_resultados(entry.get()))
+    elif tipo_busqueda == "estadisticas":
+        boton = tk.Button(ventana, text="Buscar", command = lambda:buscar_y_mostrar_estadisticas(entry.get()))
     boton.pack()
+    # Cerramos la conexión
+    conn.close()
 
-def estadisticas_jornada():
+def buscar_goles():
+    equipos_local = []
+    def filtra_equipos_local(event, data):
+        # Obtenemos el valor seleccionado
+        nonlocal combo_jornada
+        nonlocal ventana
+        nonlocal equipos_local
+        jornada = combo_jornada.get()
+        # Añadimos una lista seleccionable con los equipos locales
+        equipos_local = list(set([partido[1] for partido in data if partido[0] == int(jornada)]))
+    # Conexión a la base de datos
+    conn = sqlite3.connect('resultados.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM jornadas")
+    # Lo convertimos en una lista
+    data = cursor.fetchall()
+    # Sacamos una lista de las jornadas
+    jornadas = list(set([jornada[0] for jornada in data]))
     # Creamos la ventana
     ventana = Toplevel()
-    ventana.title("Buscar Jornada")
+    ventana.title("Buscar resultados")
     # Creamos el label
-    label = tk.Label(ventana, text="Introduce el número de la jornada")
+    label = tk.Label(ventana, text="Seleccione la jornada")
     label.pack()
-    # Creamos el entry de número de jornada que solo acepta números entre 1 y 38
-    entry = tk.Entry(ventana)
-    entry.pack()
-    entry.config(validate="key", validatecommand=(entry.register(lambda text: text.isdigit() and 0 < int(text) < 39), "%P"))
+    # Añadimos una lista seleccionable con las jornadas
+    combo_jornada = ttk.Combobox(ventana, values=jornadas, state="readonly")
+    combo_jornada.pack()
+    combo_jornada.bind("<<ComboboxSelected>>", lambda event:filtra_equipos_local(event, data))
+    # Añadimos una lista seleccionable con los equipos locales
+    combo_local = ttk.Combobox(ventana, values=equipos_local, state="readonly")
+    combo_local.pack()
+    # combo.bind("<<ComboboxSelected>>", lambda event:filtra_equipos_local(event, data))
+
+
     # Creamos el boton
-    boton = tk.Button(ventana, text="Buscar", command=lambda: buscar_por_jornada(entry.get()))
-    boton.pack()
+    # boton = tk.Button(ventana, text="Buscar", command = lambda:buscar_y_mostrar_resultados(entry.get()))
+    # Cerramos la conexión
+    conn.close()
 
 # Genera la ventana principal
 ventana = tk.Tk()
@@ -157,10 +232,13 @@ boton.pack()
 boton = tk.Button(ventana, text="Listar Jornadas", command=listar_jornadas)
 boton.pack()
 # Boton para buscar jornada
-boton = tk.Button(ventana, text="Buscar Jornada", command=buscar_jornada)
+boton = tk.Button(ventana, text="Buscar Jornada", command=lambda:buscar_jornada('resultados'))
 boton.pack()
 # Botón para ver estadísticas de una jornada
-boton = tk.Button(ventana, text="Estadísticas Jornada", command=estadisticas_jornada)
+boton = tk.Button(ventana, text="Estadísticas Jornada", command=lambda:buscar_jornada('estadisticas'))
+boton.pack()
+# Botón para buscar goles
+boton = tk.Button(ventana, text="Buscar Goles", command=buscar_goles)
 boton.pack()
 
 ventana.mainloop()
